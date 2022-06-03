c5245dde6a7c160cef32394af0541ba0d2bff28c
QianTangx <105906384+QianTangx@users.noreply.github.com>
2022-06-03 23:28:30 +0800

    Add files via upload

---
+++ ES6/第五天/2-Promise读取文件.js
@@ -0,0 +1,26 @@
+// 1. 引入 fs 模板
+const fs = require('fs');
+
+// 2. 调用方法读取文件
+fs.readFile('./resources/为学.md', 'utf-8', (err, data)=>{
+    // 如果失败，则抛出错误
+    if(err) throw err;
+    // 如果没有出错，则输入内容
+    console.log(data.toString())
+})
+
+// 3. 使用 Promise 封装
+const p = new Promise(function(resolve, reject){
+    fs.readFile('./resources/为学.md', (err, data)=>{
+        // 判断如果失败
+        if(err) reject(err)
+        // 如果成功
+        resolve(data)
+    })
+})
+
+p.then(function(value){
+    console.log(value.toString)
+},function(reason){
+    console.loh('读取失败！')
+})
